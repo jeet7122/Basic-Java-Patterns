931e8fba50f4ae1d23afec1e2d86de053f301a9c
jeet7122 <153592196+jeet7122@users.noreply.github.com>
2025-06-11 13:05:37 -0400

    Pushed All Patterns

---
+++ patterns/ReverseNumberPattern.java
@@ -0,0 +1,15 @@
+package org.example.patterns;
+
+public class ReverseNumberPattern {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i = n; i >= 1; i--)
+        {
+            for (int j = 1;  j <= i; j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
